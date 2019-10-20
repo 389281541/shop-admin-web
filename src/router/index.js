cfa7b848a1e220e11a6241f9b15bd6b74c19a1b3
38928 <389281541@qq.com>
2019-10-21 01:43:43 +0800

    首页布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Layout from '@/views/layout/Layout'
+
 Vue.use(Router)
 
 export const constantRouterMap = [
   {path: '/login', component: () => import('@/views/login/index'), hidden: true},
   {path: '/404', component: () => import('@/views/404'), hidden: true},
+  {
+    path: '',
+    component: Layout,
+    redirect: '/home',
+    children: [{
+      path: 'home',
+      name: 'home',
+      component: () => import('@/views/home/index'),
+      meta: {title: '首页', icon: 'home'}
+    }]
+  },
   {path: '*', redirect: '/404', hidden: true}
 ]
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+  .app-container {
+    margin-top: 40px;
+    margin-left: 120px;
+    margin-right: 120px;
+  }
+</style>
