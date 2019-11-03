d47364064bf30180e648ee26528a76737d051f5c
38928 <389281541@qq.com>
2019-11-04 00:24:39 +0800

    品牌和类别

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,62 @@ export const constantRouterMap = [
       meta: {title: '首页', icon: 'home'}
     }]
   },
+  {
+    path: '/item',
+    component: Layout,
+    redirect: '/item/index',
+    name: 'item',
+    meta: {title: '类别', icon: 'item'},
+    children: [
+      {
+        path: 'index',
+        name: 'index',
+        component: () => import('@/views/item/index'),
+        meta: {title: '类别列表', icon: 'item-list'}
+      },
+      {
+        path: 'update',
+        name: 'update',
+        component: () => import('@/views/item/update'),
+        meta: {title: '类别修改', icon: 'item-list'},
+        hidden: true
+      },
+      {
+        path: 'add',
+        name: 'add',
+        component: () => import('@/views/item/add'),
+        meta: {title: '添加类别', icon: 'item-add'}
+      }
+    ]
+  },
+  {
+    path: '/brand',
+    component: Layout,
+    redirect: '/brand/index',
+    name: 'brand',
+    meta: {title: '品牌', icon: 'brand'},
+    children: [
+      {
+        path: 'index',
+        name: 'index',
+        component: () => import('@/views/brand/index'),
+        meta: {title: '品牌列表', icon: 'brand-list'}
+      },
+      {
+        path: 'update',
+        name: 'update',
+        component: () => import('@/views/brand/update'),
+        meta: {title: '品牌更新', icon: 'brand-list'},
+        hidden: true
+      },
+      {
+        path: 'add',
+        name: 'add',
+        component: () => import('@/views/brand/add'),
+        meta: {title: '添加品牌', icon: 'brand-add'}
+      }
+    ]
+  },
   {path: '*', redirect: '/404', hidden: true}
 ]
 
